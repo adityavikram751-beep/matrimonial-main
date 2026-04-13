93223da47054e911fc294b087e42aa65c71a91d5
Aditya Vikram <adityavikram751@gmail.com>
2026-04-13 16:05:13 +0530

    giiii

--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -17,6 +17,9 @@ self.__BUILD_MANIFEST = {
   "/manageusers": [
     "static/chunks/pages/manageusers.js"
   ],
+  "/manageusers/[id]": [
+    "static/chunks/pages/manageusers/[id].js"
+  ],
   "/profileDetails": [
     "static/chunks/pages/profileDetails.js"
   ],
